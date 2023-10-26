f76e97bd10b28721dc727eab0f534a17472e2cd5
bsdayo <bsdayo@outlook.com>
2023-10-26 20:45:00 +0800

    docs: add github link

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,4 +4,8 @@ export default defineConfig({
   lang: 'zh-CN',
   title: 'BsIndex',
   description: '不是的导航站',
+
+  themeConfig: {
+    socialLinks: [{ icon: 'github', link: 'https://github.com/bsdayo' }],
+  },
 })
